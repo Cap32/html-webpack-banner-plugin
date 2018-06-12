73098ac817357b23376183d3f2539edb6908f15e
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-06-12 14:49:59 +0200

    Updated the code so that this plugin can work with webpack 4 as well as be backwards compatible with previous webpack versions. Based on how it was handled for html-webpack-plugin (https://github.com/jantimon/html-webpack-plugin/commit/2288f20ee3185c2ace0c8e97a1c32e2a7e11f0e0).

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
-
 'use strict';
 
-function HtmlWebpackBannerPlugin(options) {
+function HtmlWebpackBannerPlugin(options)
+{
 	this.options = options || {};
 }
 
-HtmlWebpackBannerPlugin.prototype.apply = function (compiler) {
+HtmlWebpackBannerPlugin.prototype.apply = function (compiler)
+{
 	var banner = this.options.banner || '';
 	var raw = this.options.raw;
 	var event = 'html-webpack-plugin-after-html-processing';
-	compiler.plugin('compilation', function (compilation) {
-		compilation.plugin(event, function (htmlPluginData, callback) {
+	var hook = 'htmlWebpackPluginAfterHtmlProcessing';
+
+	// Backwards compatible
+	(compiler.hooks ?
+			compiler.hooks.compilation.tap.bind(compiler.hooks.compilation, 'HtmlWebpackBannerPlugin') :
+			compiler.plugin.bind(compiler, 'compilation')
+	)(function (compilation)
+	{
+		// Backwards compatible
+		(compiler.hooks ?
+				compilation.hooks[hook].tap.bind(compilation.hooks[hook], 'HtmlWebpackBannerPlugin') :
+				compilation.plugin.bind(compilation, event)
+		)(function (htmlPluginData, callback)
+		{
 			var comment = banner && !raw ? '<!--' + banner + '-->\n' : banner;
+
 			htmlPluginData.html = comment + htmlPluginData.html;
-			callback(null, htmlPluginData);
+
+			if(callback)
+			{
+				callback(null, htmlPluginData);
+			}
+			else
+			{
+				return htmlPluginData;
+			}
 		});
-	});
+	})
 };
 
 module.exports = HtmlWebpackBannerPlugin;
